2f9f0ed2026dcae95b72211052ba1727f992bb68
osaguild <nobuhisa.abe.0922@gmail.com>
2022-05-20 08:45:27 +0900

    remove .docusaurus from git

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/Users/akira/frame00/docs.devprotocol.xyz/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/akira/frame00/docs.devprotocol.xyz/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/akira/frame00/docs.devprotocol.xyz/node_modules/@docusaurus/theme-classic/lib/admonitions.css'),
-  require('/Users/akira/frame00/docs.devprotocol.xyz/src/css/custom.css'),
-];
