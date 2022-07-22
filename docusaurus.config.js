4a7d2e06e3473e984ce6d38936ec12d7a4d67201
osaguild <nobuhisa.abe.0922@gmail.com>
2022-07-22 21:11:31 +0900

    fix index name for test

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -247,7 +247,7 @@ const config = {
 		algolia: {
 			appId: process.env.ALGOLIA_APP_ID,
 			apiKey: process.env.ALGOLIA_API_KEY,
-			indexName: process.env.ALGOLIA_INDEX_NAME,
+			indexName: 'crawler_devdocs',
 			contextualSearch: false,
 			algoliaOptions: {},
 		},
